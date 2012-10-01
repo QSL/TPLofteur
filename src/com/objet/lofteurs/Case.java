8e23ac6613428d7fefa2ab4dc2312188bcb809d0
QSL <QSL@QSL-PC>
2012-10-01 17:53:19 +0200

    Ecriture des attributs et méthodes pour Loft Case

--- src/com/objet/lofteurs/Case.java
+++ src/com/objet/lofteurs/Case.java
@@ -9,5 +9,30 @@ package com.objet.lofteurs;
  * @author QSL
  */
 public class Case {
+    protected int abscisse;
+    protected int ordonnee;
     
+    public Neuneu getNeuneu(){
+        
+    }
+    
+    public Nourriture getNourriture(){
+        
+    }
+    
+    public void addNeuneu(Neuneu n){
+        
+    }
+    
+    public void removeNeuneu(Neuneu n){
+        
+    }
+    
+    public void addNourriture(Nourriture alimentation){
+        
+    }
+    
+    public void removeNourriture(Nourriture alimentation){
+        
+    }
 }
--- src/com/objet/lofteurs/Loft.java
+++ src/com/objet/lofteurs/Loft.java
@@ -10,7 +10,47 @@ package com.objet.lofteurs;
  */
 class Loft {
 
+    protected Neuneu[] population;
+    protected int largeur;
+    protected int hauteur;
+    protected Case[][] listcases;
+    
     public Loft(int tailleLoft, ZoneGraphique zone) {
     }
     
+    public void remplissageAleatoire(int nb_neuneu, int nb_nourr){
+        
+    }
+    
+    public void addNeuneu(Neuneu lofteur){
+        
+    }
+    
+    public void addNourriture(Nourriture Consommable){
+        
+    }
+    
+    public Case getContenuCase(){
+        
+    }
+    
+    public void lancement(int tours){
+        
+    }
+    
+    public Neuneu[] getPopulation(){
+        
+    }
+    
+    public void setPopulation(Neuneu[] listNeuneu){
+        
+    }
+    
+    public Case[][] getListCases(){
+        
+    }
+    
+    public void setListeCases(Case[] listeCase){
+        
+    }
 }
