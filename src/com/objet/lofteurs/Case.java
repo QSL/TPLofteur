ca9adad2f157dd61d047250f9045d7cd0d556761
QSL <QSL@QSL-PC>
2012-10-23 17:24:52 +0200

    ajout des commentaires pour la classe case

--- src/com/objet/lofteurs/Case.java
+++ src/com/objet/lofteurs/Case.java
@@ -14,6 +14,7 @@ public class Case {
     protected int abscisse;
     protected int ordonnee;
     
+    // méthode permettant de retourner la liste des neuneus qui sont sur la case
     public List<Neuneu> getNeuneu(){
         List<Neuneu> listeDeNeuneuSurLaCase = new ArrayList<>();
         for(int i=0; i<Loft.population.length; i++)
@@ -27,23 +28,31 @@ public class Case {
         
     }
     
+    // permet de retourner la nourriture sur la case
+    // non implémentée car possiblement non utile
     public Nourriture getNourriture(){
         return null;
         
     }
     
+    // ajouter un neuneu sur la case
     public void addNeuneu(Neuneu n){
         n.setPosition(this);
     }
     
+    // enlever un neuneu de la case
+    // non implémentée car possiblement non utile
     public void removeNeuneu(Neuneu n){
         
     }
     
+    // ajouter de la nourriture sur la case
     public void addNourriture(Nourriture alimentation){
         alimentation.setPosition(this);
     }
     
+    // enlever de la nourriture de la case
+    // non implémentée car possiblement non utile
     public void removeNourriture(Nourriture alimentation){
         
     }
